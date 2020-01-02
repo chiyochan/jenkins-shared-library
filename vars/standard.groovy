b2c3c1642014cab4ff546de3de47daf1557e4328
chiyochan <chiyochan9@gmail.com>
2020-01-02 07:40:42 -0700

    adding library

--- vars/standard.groovy
+++ vars/standard.groovy
@@ -1,5 +1,4 @@
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+def call() {
+    echo "Build"
+    bat 'gradle clean build'
 }
